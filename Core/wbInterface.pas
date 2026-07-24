6c111f40871989584cf0845aa3243b568104bb9f
robertgk2017 <robertgk2017@gmail.com>
2026-07-23 22:25:31 -0400

    Interface - Expose overload of wbContainerHandler.ContainerExists

--- Core/wbInterface.pas
+++ Core/wbInterface.pas
@@ -3283,7 +3283,8 @@ type
     function OpenResource(const aFileName: string): TDynResources;
     function OpenResourceData(const aContainerName, aFileName: string): TBytes;
 
-    function ContainerExists(const aContainerName: string): Boolean;
+    function ContainerExists(const aContainerName: string): Boolean; overload;
+    function ContainerExists(const aContainerName: string; out aContainer: IwbResourceContainer): Boolean; overload;
     procedure ContainerList(const aList: TStrings);
     procedure ContainerResourceList(const aContainerName: string; const aList: TStrings; const aFolder: string = '');
     procedure ContainerResourceDict(const aContainerName: string; const aDict: TwbResourceDict; const aFolder: string = '');
