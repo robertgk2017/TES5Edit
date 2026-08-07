91c130073215e2c2b8f0a646991e437395b948b4
robertgk2017 <robertgk2017@gmail.com>
2026-08-07 13:54:29 -0400

    BSArch - fix Misc files flag setting in newly created FO3/FNV/TES5 archives

--- Core/wbBSArchive.pas
+++ Core/wbBSArchive.pas
@@ -1723,7 +1723,7 @@ begin
     // fixes to avoid game crashes with wrong flags
     // embedded names in textures only archives, might crash on other files
     if not bTexturesOnly then
-      fHeader.FileFlags := fHeader.FileFlags and not ARCHIVE_EMBEDNAME
+      fHeader.Flags := fHeader.Flags and not ARCHIVE_EMBEDNAME
     // SSE crashing bug - textures with embedded names must be compressed
     else if not Assigned(fPacker) and (fType = baSSE) and (fHeader.Flags and ARCHIVE_EMBEDNAME <> 0) then
       for var f in Self do f.Compress := True;
