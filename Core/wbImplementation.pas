98b149a813d2aaf8b6399c6b5ef06bfdda026ca3
robertgk2017 <robertgk2017@gmail.com>
2026-08-09 05:58:47 -0400

    FO76 - And Form Version to 209

--- Core/wbImplementation.pas
+++ Core/wbImplementation.pas
@@ -10128,7 +10128,7 @@ var
     if wbGameMode >= gmFO3 then begin
       case wbGameMode of
         gmSF1                        : BasePtr.mrsVersion^ := 582;
-        gmFO76                       : BasePtr.mrsVersion^ := 208;
+        gmFO76                       : BasePtr.mrsVersion^ := 209;
         gmFO4, gmFO4VR               : BasePtr.mrsVersion^ := 131;
         gmSSE, gmTES5VR, gmEnderalSE : BasePtr.mrsVersion^ := 44;
         gmTES5, gmEnderal            : BasePtr.mrsVersion^ := 43;
