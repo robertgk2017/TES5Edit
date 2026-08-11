c63b1e9ec33bb415247cc619693e691eb1f4a395
robertgk2017 <robertgk2017@gmail.com>
2026-08-11 14:05:59 -0400

    xDump - add wbVWDAsQuestChildren to FO4/FO76/SF1

--- xDump.dpr
+++ xDump.dpr
@@ -1082,7 +1082,7 @@ begin
           end;
         end;
         gmTES5: begin
-          wbGameName := 'Skyrim';
+          wbGameName    := 'Skyrim';
           wbGameExeName := 'TESV';
           case wbToolSource of
             tsSaves:   DefineTES5Saves;
@@ -1090,8 +1090,8 @@ begin
           end;
         end;
         gmEnderal: begin
-          wbGameName := 'Enderal';
-          wbGameExeName := 'TESV';
+          wbGameName      := 'Enderal';
+          wbGameExeName   := 'TESV';
           wbGameMasterEsm := 'Skyrim.esm';
           case wbToolSource of
             tsSaves:   DefineTES5Saves;
@@ -1099,8 +1099,8 @@ begin
           end;
         end;
         gmTES5VR: begin
-          wbGameName := 'Skyrim';
-          wbGameName2 := 'Skyrim VR';
+          wbGameName    := 'Skyrim';
+          wbGameName2   := 'Skyrim VR';
           wbGameExeName := 'SkyrimVR';
           tss := [tsPlugins];
           case wbToolSource of
@@ -1109,19 +1109,21 @@ begin
           end;
         end;
         gmFO4: begin
-          wbGameName := 'Fallout4';
-          wbCreateContainedIn := False;
+          wbGameName           := 'Fallout4';
+          wbCreateContainedIn  := False;
+          wbVWDAsQuestChildren := True;
           case wbToolSource of
             tsSaves:   DefineFO4Saves;
             tsPlugins: DefineFO4;
           end;
         end;
         gmFO4VR: begin
-          wbGameName := 'Fallout4';
-          wbGameExeName := 'Fallout4VR';
-          wbGameName2 := 'Fallout4VR';
-          wbGameNameReg := 'Fallout 4 VR';
-          wbCreateContainedIn := False;
+          wbGameName           := 'Fallout4';
+          wbGameExeName        := 'Fallout4VR';
+          wbGameName2          := 'Fallout4VR';
+          wbGameNameReg        := 'Fallout 4 VR';
+          wbCreateContainedIn  := False;
+          wbVWDAsQuestChildren := True;
           tss := [tsPlugins];
           case wbToolSource of
             //tsSaves:   DefineFO4Saves;
@@ -1129,20 +1131,20 @@ begin
           end;
         end;
         gmSSE: begin
-          wbGameName := 'Skyrim';
+          wbGameName    := 'Skyrim';
           wbGameExeName := 'SkyrimSE';
-          wbGameName2 := 'Skyrim Special Edition';
+          wbGameName2   := 'Skyrim Special Edition';
           case wbToolSource of
             tsSaves:   DefineTES5Saves;
             tsPlugins: DefineTES5;
           end;
         end;
         gmEnderalSE: begin
-          wbAppName := 'EnderalSE';
-          wbGameName := 'Enderal';
-          wbGameExeName := 'SkyrimSE';
-          wbGameName2 := 'Enderal Special Edition';
-          wbGameNameReg := 'EnderalSE';
+          wbAppName       := 'EnderalSE';
+          wbGameName      := 'Enderal';
+          wbGameExeName   := 'SkyrimSE';
+          wbGameName2     := 'Enderal Special Edition';
+          wbGameNameReg   := 'EnderalSE';
           wbGameMasterEsm := 'Skyrim.esm';
           case wbToolSource of
             tsSaves:   DefineTES5Saves;
@@ -1150,18 +1152,20 @@ begin
           end;
         end;
         gmFO76: begin
-          wbGameName := 'Fallout76';
-          wbGameNameReg := 'Fallout 76';
-          wbGameMasterEsm := 'SeventySix.esm';
-          wbCreateContainedIn := False;
+          wbGameName           := 'Fallout76';
+          wbGameNameReg        := 'Fallout 76';
+          wbGameMasterEsm      := 'SeventySix.esm';
+          wbCreateContainedIn  := False;
+          wbVWDAsQuestChildren := True;
           tss := [tsPlugins];
           case wbToolSource of
             tsPlugins: DefineFO76;
           end;
         end;
         gmSF1: begin
-          wbGameName := 'Starfield';
-          wbCreateContainedIn := False;
+          wbGameName           := 'Starfield';
+          wbCreateContainedIn  := False;
+          wbVWDAsQuestChildren := True;
           case wbToolSource of
             tsPlugins: DefineSF1;
           end;
