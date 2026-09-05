6dbbed51d92456e39ffb916eece56040e91114b5
ElminsterAU <github@thorsten.engler.id.au>
2026-09-06 02:46:24 +1000

    inherited Create in TThread does not Start a thread that was created with CreateSuspended=False, that only happens in AfterConstruction. So passing CreateSuspended True and calling Start in Create after calling inherited Create then crashes in AfterConstruction.

--- xEdit/xeMainForm.pas
+++ xEdit/xeMainForm.pas
@@ -20990,9 +20990,8 @@ begin
   ltLoadList := aList;
   aList := nil;
   ltStates := aFileStates;
-  inherited Create(True);
+  inherited Create(False);
   FreeOnTerminate := True;
-  Start;
 end;
 
 constructor TLoaderThread.Create(const aFileName, aMaster: string; aLoadOrder: Integer; aFileStates: TwbFileStates = []);
@@ -21003,9 +21002,8 @@ begin
   ltLoadList.Add(aFileName);
   ltMaster := aMaster;
   ltStates := aFileStates;
-  inherited Create(True);
+  inherited Create(False);
   FreeOnTerminate := True;
-  Start;
 end;
 
 destructor TLoaderThread.Destroy;
